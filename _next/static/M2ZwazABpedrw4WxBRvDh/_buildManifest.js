e557f8a2610ae55cdb980c772b23652009508bbd
howden <howden@users.noreply.github.com>
2022-01-01 16:36:56 +0000

    deploy: 9b7f1f64521b02446e06f957e44875aa75a68e2c

---
+++ _next/static/M2ZwazABpedrw4WxBRvDh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6d6379e24bf1ef10.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
